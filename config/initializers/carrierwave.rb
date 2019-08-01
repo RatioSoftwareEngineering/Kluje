3798f170ce90dc047753a69badabd8a226b279d2
Nikita Andreyev <msa31415@gmail.com>
2019-08-01 21:45:41 +0300

    works locally

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,14 +5,14 @@ CarrierWave.configure do |config|
   # config.asset_host = 's3-ap-southeast-1.amazonaws.com'
   # config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
-  if Rails.env == 'development' || Rails.env == 'test'
-     config.aws_bucket = 'kluje-development'
-     config.aws_credentials = {
-       aws_access_key_id: Kluje.settings['aws']['access_key_id'],
-       aws_secret_access_key: Kluje.settings['aws']['secret_access_key'],
-       s3_endpoint: 's3-ap-southeast-1.amazonaws.com'
-    }
-  else
+  # if Rails.env == 'development' || Rails.env == 'test'
+  #    config.aws_bucket = 'kluje-development'
+  #    config.aws_credentials = {
+  #      aws_access_key_id: Kluje.settings['aws']['access_key_id'],
+  #      aws_secret_access_key: Kluje.settings['aws']['secret_access_key'],
+  #      s3_endpoint: 's3-ap-southeast-1.amazonaws.com'
+  #   }
+  # else
     if Kluje.settings['assets']
       config.asset_host = Kluje.settings['assets']['s3']
     end
@@ -26,5 +26,5 @@ CarrierWave.configure do |config|
     else
       config.aws_bucket = 'kluje-staging'
     end
-  end
+  # end
 end
